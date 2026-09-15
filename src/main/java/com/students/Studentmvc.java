02cd702ecb406c73e82880e430a526265adf1f08
Moniye-lang <davidadeniyi269@gmail.com>
2026-09-15 10:40:31 +0100

    Initial StudentMVC project

---
+++ src/main/java/com/students/Studentmvc.java
@@ -0,0 +1,12 @@
+package com.students;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Studentmvc {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Studentmvc.class, args);
+    }
+}
